d69d2723023b2fe0418e9c06514132acc07bbbab
Timothée Jourde <itim.lcf@gmail.com>
2018-12-28 03:40:43 +0100

    switch to non-linear color scale

--- src/main/scala/ColorScale.scala
+++ src/main/scala/ColorScale.scala
@@ -3,56 +3,52 @@ import scala.math._
 object ColorScale {
   // Challenger Deep: -11034m
   // Mount Everest  : +8848m
-  val halfRange = 8848
 
   // Extracted from:
   // https://www.r-bloggers.com/gmt-standard-color-palettes
   val relief = ColorScale(Vector(
-    (0,0,0),
-    (1,6,16),
-    (0,7,30),
-    (3,27,63),
-    (9,74,121),
-    (16,131,188),
-    (59,176,173),
-    (123,222,158),
-    (176,250,171),
-    (212,252,212),
-    (56,103,38),
-    (117,98,43),
-    (161,141,56),
-    (210,189,71),
-    (248,228,87),
-    (248,233,107),
-    (251,235,129),
-    (251,240,151),
-    (251,245,180),
-    (252,250,217)
+    -8000 -> (0,0,0),
+    -4000 -> (1,6,16),
+    -2000 -> (0,7,30),
+    -1000 -> (3,27,63),
+    -100  -> (9,74,121),
+    -60   -> (16,131,188),
+    -30   -> (59,176,173),
+    -20   -> (123,222,158),
+    -10   -> (176,250,171),
+    -5    -> (212,252,212),
+     5    -> (56,103,38),
+     10   -> (117,98,43),
+     20   -> (161,141,56),
+     30   -> (210,189,71),
+     60   -> (248,228,87),
+     100  -> (248,233,107),
+     1000 -> (251,235,129),
+     2000 -> (251,240,151),
+     4000 -> (251,245,180),
+     8000 -> (252,250,217)
   ))
 }
 
-case class ColorScale(palette: Vector[Tuple3[Int,Int,Int]]) {
+case class ColorScale(palette: Vector[ Tuple2[Int, Tuple3[Int,Int,Int]] ]) {
   import ColorScale._
 
-  def apply(w: Short) = {
-    val lo    = -halfRange
-    val hi    = halfRange
-    val x     = max(lo, min(w.toInt, hi))
-    val range = 2 * halfRange
-    val maxI  = palette.length - 1
+  def apply(x: Short) = {
+    val pairs = palette.sliding(2).toVector
+    val Vector(from,to) = pairs
+      .find{ case Vector(lo,hi) => x <= hi._1 }
+      .getOrElse(pairs.last)
 
-    val fromI = ((x-lo) * maxI) / range
-    val modI  = ((x-lo) * maxI) % range
-    val toI   = min(fromI + 1, maxI)
+    val lo    = min(x, from._1)
+    val hi    = max(x, to._1)
+    val range = hi - lo
 
-    val from  = palette(fromI)
-    val to    = palette(toI)
+    val k = x - lo
+    def lerp(a:Int, b:Int) = a*(range-k)/range + b*k/range
 
-    def lerp(a:Int, b:Int) = a*(range-modI-1)/(range-1) + b*modI/(range-1)
-
-    ( lerp(from._1, to._1),
-      lerp(from._2, to._2),
-      lerp(from._3, to._3) )
+    ( lerp(from._2._1, to._2._1),
+      lerp(from._2._2, to._2._2),
+      lerp(from._2._3, to._2._3) )
   }
 }
 

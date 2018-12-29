37531a060fb584ee0df78eec230547820f1f16f1
Timothée Jourde <itim.lcf@gmail.com>
2018-12-29 01:13:22 +0100

    tile down-scaling

--- src/main/scala/Tile.scala
+++ src/main/scala/Tile.scala
@@ -16,15 +16,37 @@ object Tile {
           if (i / rawSize == 0 || i % rawSize == 0)
             None
           else
-            Some((((hi & 0xFF) << 8) | (lo & 0xFF)).toShort)
+            Some(( ((hi & 0xFF) << 8) | (lo & 0xFF) ).toShort)
         }
 
       Tile(Some(data.toVector), pos)
   }
 
   def apply(upscaled: Array[Tile]): Tile = {
-    /* TODO: scale down (with linear interpolation) */
-    Tile(Some(Vector()), upscaled.head.position.scaleDown)
+    def at(x2: Int, y2: Int) = {
+      val x0 = x2 / size
+      val y0 = y2 / size
+
+      val x1 = x2 - (size*x0)
+      val y1 = y2 - (size*y0)
+
+      upscaled(y0*2 + x0)(x1, y1).toInt
+    }
+    val data = Vector.tabulate(size*size){ i =>
+      val x2_0 = (i % size) * 2
+      val y2_0 = (i / size) * 2
+
+      val x2_1 = x2_0 + 1
+      val y2_1 = y2_0 + 1
+
+      val a = at(x2_0, y2_0)
+      val b = at(x2_0, y2_1)
+      val c = at(x2_1, y2_0)
+      val d = at(x2_1, y2_1)
+
+      ((a + b + c + d) / 4).toShort
+    }
+    Tile(Some(data), upscaled.head.position.scaleDown)
   }
 }
 

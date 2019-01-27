c3deb3d154cf4b65e68684b77c5c522d061f54a3
Timothée Jourde <itim.lcf@gmail.com>
2019-01-27 16:15:36 +0100

    [UNTESTED] HadoopFs

--- src/main/scala/Fs.scala
+++ src/main/scala/Fs.scala
@@ -1,6 +1,10 @@
-import org.apache.spark.rdd._
-import java.nio.file._
 import collection.JavaConverters._
+
+import sun.misc.IOUtils;
+import java.nio.file.{Files, Paths}
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs._
 
 trait Fs {
   def readDirectory(path: String): Seq[String]
@@ -21,4 +25,26 @@ class LocalFs extends Fs {
 
   override def writeFile(path: String, data: Vector[Byte]) =
     Files.write(Paths.get(path), data.toArray)
+}
+
+class HadoopFs(cfg: Configuration) extends Fs {
+  private val fs = FileSystem.get(cfg)
+
+  private def toList(it: RemoteIterator[LocatedFileStatus]): List[String] =
+    if (it.hasNext)
+      it.next.getPath.toString :: toList(it)
+    else
+      List()
+
+  override def readDirectory(path: String) =
+    toList( fs.listFiles(new Path(path), false) )
+
+  override def readFile(path: String) = {
+    val stream = fs.open(new Path(path))
+    val res    = IOUtils.readFully(stream, -1, false).toVector
+    stream.close
+    res
+  }
+
+  override def writeFile(path: String, data: Vector[Byte]) = ???
 }
